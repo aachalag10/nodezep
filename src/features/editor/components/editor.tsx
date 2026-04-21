dede517675169a74f6c526f0e98bde427e0d7c9a
aachalag10 <aachalag75@gmail.com>
2026-04-21 12:42:30 +0530

    xyflow/react

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,20 +1,40 @@
-"use client"
-import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows"
-import { ErrorView, LoadingView } from "@/components/entity-components"
+"use client";
+import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
+import { ErrorView, LoadingView } from "@/components/entity-components";
+import { useState, useCallback } from "react";
+import {
+  ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  type Node,
+  type Edge,
+} from "@xyflow/react";
+// import "@xyflow/react/dist/style.css";
 
-export const EditorLoading=()=>{
-    return <LoadingView message="Loading editor..."/>
-}
+export const EditorLoading = () => {
+  return <LoadingView message="Loading editor..." />;
+};
 
-export const EditorError=()=>{
-    return <ErrorView message="Failed to load editor"/>
-}
+export const EditorError = () => {
+  return <ErrorView message="Failed to load editor" />;
+};
 
-export const Editor=({workflowId}:{workflowId:string})=>{
-    const {data:workflow}=useSuspenseWorkflow(workflowId);
-    return(
-       <p>
-        {JSON.stringify(workflow,null,2)}
-       </p>
-    )
-}
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+export const Editor = ({ workflowId }: { workflowId: string }) => {
+  const { data: workflow } = useSuspenseWorkflow(workflowId);
+
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  return <p>{JSON.stringify(workflow, null, 2)}</p>;
+};
