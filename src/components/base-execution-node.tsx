1e54952d1f3c1f88bd26228eadda1a94958d5652
aachalag10 <aachalag75@gmail.com>
2026-04-22 13:09:43 +0530

    base-handle

--- src/components/base-execution-node.tsx
+++ src/components/base-execution-node.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { type NodeProps, Position } from "@xyflow/react";
-import type { LucideIcon } from "lucide-react";
+import { Icon, type LucideIcon } from "lucide-react";
 import Image from "next/image";
-import { memo, type ReactNode, useCallback } from "react";
+import { memo, type ReactNode } from "react";
 import { BaseNode, BaseNodeContent } from "@/components/react-flow/base-node";
-import { BaseHandle } from "./react-flow/base-handle";
 import { WorkflowNode } from "./workflow-node";
+import { BaseHandle } from "@/components/react-flow/base-handle";
 
 interface BaseExecutionNodeProps extends NodeProps {
   icon: LucideIcon | string;
@@ -17,8 +17,41 @@ interface BaseExecutionNodeProps extends NodeProps {
   onDoubleClick?: () => void;
 }
 
-// export const BaseExecutionNode=memo(
-//     ({
-//         id,icon,name,description
-//     })
-// )
+export const BaseExecutionNode = memo(
+  ({
+    id,
+    icon: Icon,
+    name,
+    description,
+    children,
+    onSettings,
+    onDoubleClick,
+  }: BaseExecutionNodeProps) => {
+    //TODO:add delete method
+    const handleDelete = () => {};
+    return (
+      <WorkflowNode
+        name={name}
+        description={description}
+        onDelete={handleDelete}
+        onSettings={onSettings}
+      >
+        {/* TODO:Wrap within NodeStatusIndicator */}
+
+        <BaseNode onDoubleClick={onDoubleClick}>
+          <BaseNodeContent>
+            {typeof Icon === "string" ? (
+              <Image src={Icon} alt={name} width={16} height={16} />
+            ) : (
+              <Icon className="size-4 text-muted-foreground" />
+            )}
+            {children}
+            <BaseHandle id="target-1" type="target" position={Position.Left} />
+            <BaseHandle id="source-1" type="source" position={Position.Right} />
+          </BaseNodeContent>
+        </BaseNode>
+      </WorkflowNode>
+    );
+  },
+);
+BaseExecutionNode.displayName = "BaseExecutionNode";
