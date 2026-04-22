2edc994cd062d20fe46827164a37df207e2098c2
aachalag10 <aachalag75@gmail.com>
2026-04-22 12:06:12 +0530

    Base-execution-node

--- src/components/base-execution-node.tsx
+++ src/components/base-execution-node.tsx
@@ -1,7 +1,24 @@
-"use client"
+"use client";
 
-import {type NodeProps,Position} from "@xyflow/react";
+import { type NodeProps, Position } from "@xyflow/react";
 import type { LucideIcon } from "lucide-react";
 import Image from "next/image";
-import {memo,type ReactNode,useCallback} from "react";
-import {BaseNode,BaseNodeContent} from "@/components/react-flow/base-node";
+import { memo, type ReactNode, useCallback } from "react";
+import { BaseNode, BaseNodeContent } from "@/components/react-flow/base-node";
+import { BaseHandle } from "./react-flow/base-handle";
+import { WorkflowNode } from "./workflow-node";
+
+interface BaseExecutionNodeProps extends NodeProps {
+  icon: LucideIcon | string;
+  name: string;
+  description?: string;
+  children?: ReactNode;
+  onSettings?: () => void;
+  onDoubleClick?: () => void;
+}
+
+// export const BaseExecutionNode=memo(
+//     ({
+//         id,icon,name,description
+//     })
+// )
